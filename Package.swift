d8d5dfb151748ac630417d463e6353756ddcf2fd
Adrian <adrian@Adrians-MacBook-Air.local>
2025-07-26 13:23:06 +0100

    fixing duff package

--- Package.swift
+++ Package.swift
@@ -1,26 +1,21 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "WSwiftUI",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "WSwiftUI",
-            targets: ["WSwiftUI"]),
-        .executable(name: "WSwiftUITest", targets: ["WSwiftUITest"])
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "WSwiftUI"),
-        .target(name: "WSwiftUITest", dependencies: ["WSwiftUI"]),
-        .testTarget(
-            name: "WSwiftUITests",
-            dependencies: ["WSwiftUI"]
-        ),
-    ],
-    swiftLanguageModes: [.v5]
+  name: "WSwiftUI",
+  platforms: [
+    .iOS(.v13), .macOS(.v10_15)
+  ],
+  products: [
+    .library(name: "WSwiftUI", targets: ["WSwiftUI"]),
+  ],
+  dependencies: [
+    // …
+  ],
+  targets: [
+    .target(name: "WSwiftUI", path: "Sources/WSwiftUI"),
+  ],
+  swiftLanguageVersions: [.v5]            // <- explicitly use the 5.x initializer
 )
